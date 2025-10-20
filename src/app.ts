c5bdafe1da4940d790f69b5a2fa30ecbc9752c75
pquaidoo <pquaid@uw.edu>
2025-10-19 20:32:03 -0700

    docs route added fixes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 // src/app.ts
 import express from 'express';
 import { tvShowRoutes } from './routes/open/tvShowRoutes';
+import { docsRoutes } from './routes/open/docsRoutes';
 
 const app = express();
 app.use(express.json());
 
 // Health (GET /health)
 app.get('/health', (_req, res) => res.json({ status: 'API is running' }));
-
+app.use(docsRoutes);
 // Mount open routes
 app.use('/api', tvShowRoutes);
 
--- src/routes/open/index.ts
+++ src/routes/open/index.ts
@@ -2,12 +2,10 @@
 import { Router } from 'express';
 import { apiKeyAuth } from '../../core/middleware/apiKeyAuth';
 import { tvShowRoutes } from './tvShowRoutes';
-import { docsRoutes } from './docsRoutes';
 
 
 export const openRouter = Router();
 openRouter.use(tvShowRoutes);
 
-openRouter.use(docsRoutes);
 
 openRouter.use('/api', tvShowRoutes);
